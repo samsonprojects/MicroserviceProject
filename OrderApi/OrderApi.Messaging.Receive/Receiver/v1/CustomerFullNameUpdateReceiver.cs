0d457412dc7ef53cc95618339786bfe9e6e1af40
Sam <sam@iamtheagent.com>
2022-02-21 23:38:36 +0000

    start of rabbitmq

---
+++ OrderApi/OrderApi.Messaging.Receive/Receiver/v1/CustomerFullNameUpdateReceiver.cs
@@ -0,0 +1,9 @@
+using Microsoft.Extensions.Hosting;
+
+namespace OrderApi.Messaging.Receive.Receiver.v1
+{
+    public class CustomerFullNameUpdateReceiver : BackgroundService
+    {
+        
+    }
+}
